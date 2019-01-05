9af5fdb21cf26e1128b681e5e63606c91833bc2b
Aqdam Naseem <aqdmnsm1988@gmail.com>
2019-01-05 18:53:16 +0400

    Added code to reverse linked list

---
+++ Linked List/src/com/aqdamnaseem/projects/linkedlist/Problem1/LinkedList.java
@@ -0,0 +1,95 @@
+package com.aqdamnaseem.projects.linkedlist.Problem1;
+
+public class LinkedList {
+
+	private Node head;
+
+	/**
+	 * Recursive approach to reverse a linked list
+	 */
+	public void recursiveReverse() {
+		if (head == null)
+			return;
+		reverseUtil(head);
+	}
+
+	/**
+	 * Reverse Util
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public Node reverseUtil(Node node) {
+		if (node.next == null) {
+			head = node;
+			return node;
+		}
+		Node reversedNode = reverseUtil(node.next);
+		reversedNode.next = node;
+		node.next = null;
+		return node;
+	}
+
+	/**
+	 * Iterative approach to reverse a linked list
+	 */
+	public void iterativeReverse() {
+		if (head == null)
+			return;
+		Node curr = head;
+		Node prev = null;
+		Node next = null;
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
+	}
+
+	public void add(Integer data) {
+		head = new Node(data, head);
+	}
+
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		Node curr = head;
+		while (curr != null) {
+			result.append(curr.data).append(" ");
+			curr = curr.next;
+		}
+		return result.toString();
+	}
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(5);
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(1);
+		System.out.println("Actual List : " + list.toString());
+		list.recursiveReverse();
+		System.out.println(
+				"Reversed List with Recursive Reverse : " + list.toString());
+		list.iterativeReverse();
+		System.out.println(
+				"Reversed List with Iterative Reverse : " + list.toString());
+
+	}
+
+	private static class Node {
+
+		private Integer data;
+		private Node next;
+
+		public Node(Integer data, Node next) {
+			super();
+			this.data = data;
+			this.next = next;
+		}
+
+	}
+
+}
